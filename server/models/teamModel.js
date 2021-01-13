95ce27a71443f5fac7e603a4f985bddfd9cb83bf
VictoryJu <kob0805@naver.com>
2021-01-14 02:31:23 +0900

    modify: teamSchema

--- server/models/teamModel.js
+++ server/models/teamModel.js
@@ -3,20 +3,24 @@ const mongoose = require('mongoose');
 const teamSchema = new mongoose.Schema({
     title: { type: String, required: true, unique: true }, //팀 이름
     skills:{ type: String, required: true }, // 분야
-    people:{ type: Number, required: true } // 총 인원 수 
+    people:{ type: Number, required: true }, // 총 인원 수
+    description: {type: String, required: true}, // 모집글
+    admin: {type: String, default: true}, // 생성자
+    current:{type: Number, default: 0} // 현재 인원 
 });
 
-teamSchema.statics.createTeam = function ({ title, skills, people }) {
+teamSchema.statics.createTeam = function ({ title, skills, peoples,description }) {
     const team = new this({
         title, //팀 이름 
         skills, // 구하는 분야
-        people // 총 인원 수 
+        peoples, // 총 인원 수 
+        description // 홍보 글
     });
 
     return team.save();
 };
 
-// 보류 상의후 다시
+// 보류 상의후 다시 = 팀 스키마에 있지말고 유저 스키마로 가는게 맞는거같음
 teamSchema.static.applyTeam = function ({ title, skills }) {
     const apply = new this({
         title, // 만들어진 팀 이름
