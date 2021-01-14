99ca5f44f7ff67d39c8fb364c767fd21e8c1069c
VictoryJu <kob0805@naver.com>
2021-01-14 20:20:45 +0900

    modify: contestSchema -> projectSchema

--- server/models/contestModel.js
+++ server/models/contestModel.js
@@ -1,26 +1,39 @@
 const mongoose = require('mongoose');
 const moment = require('moment');
-const contest = require('../controllers/contestController');
 require('moment-timezone');
 moment.tz.setDefault('Asia/Seoul');
 var date = moment().format('YYYY-MM-DD HH:mm:ss');
 
-const contestSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    host: {type: String, required: true},
-    period: {type: String, required: true}, // or 마감기간
-    description: {type: String, required: true},
-    created_at: {type: String, default: date},
-    scrap: {type: Number, default: 0},
-    img_url: {type: String} // default 로 기본 사진 하나 추가해주기
+const teamSchema = new mongoose.Schema({ //SubDocument로 사용합니다. 
+    name: {type: String, required: true, unique: true}, // 팀명
+    title: {type: String, required: true}, // 팀원 모집글 title 
+    part: {type: String, required: true}, // 필요한 파트
+    total: {type: Number, required: true}, // 필요한 총 인원
+    mates: {type: Number, required: true}, // 현재 인원 수 
 });
 
-contestSchema.statics.showContestDetail = function (contestIdx) {
-    return this.find({"_id": mongoose.Types.ObjectId(contestIdx)});
+
+const projectSchema = new mongoose.Schema({
+    title: {type: String, required: true, unique: true}, // 프로젝트 이름
+    period: {type: String, required: true}, // or 마감기간
+    description: {type: String, required: true}, // 프로젝트 내용 
+    created_at: {type: String, default: date}, // 생성 시간
+    scrap: {type: Number, default: 0}, // 스크랩 수
+    img_url: {type: String}, // default 로 기본 사진 하나 추가해주기
+    teams: [teamSchema], // SubDocument
+    projectIdx: {type: Number, required: true} // 공모전, 프로젝트, 해커톤 등 구별 id 
+});
+
+
+
+projectSchema.statics.showprojectDetail = function (projectIdx) {
+    return this.find({"_id": mongoose.Types.ObjectId(projectIdx)});
 }
 
-contestSchema.statics.showTeamRecruitmentList = function (contestIdx) {
-    return this.find({"_id": mongoose.Types.ObjectId(contestIdx)}); // 팀원 관련 스키마랑 합치기
+projectSchema.statics.showTeamRecruitmentList = function (projectIdx) {
+    return this.find({"_id": mongoose.Types.ObjectId(projectIdx)}); // 팀원 관련 스키마랑 합치기
 }
 
-module.exports = mongoose.model('contest', contestSchema);
+
+
+module.exports = mongoose.model('project', projectSchema);
